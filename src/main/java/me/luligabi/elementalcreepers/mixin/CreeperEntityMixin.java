65791af972da52f4a8535b7d8666b2d1903db938
Luligabi1 <luligabi111@gmail.com>
2021-02-22 21:17:38 -0300

    Added vanilla explosion behavior into CreeperEntity's Mixin.

--- src/main/java/me/luligabi/elementalcreepers/mixin/CreeperEntityMixin.java
+++ src/main/java/me/luligabi/elementalcreepers/mixin/CreeperEntityMixin.java
@@ -1,11 +1,17 @@
 package me.luligabi.elementalcreepers.mixin;
 
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.mob.CreeperEntity;
+import net.minecraft.world.GameRules;
+import net.minecraft.world.explosion.Explosion;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 
 @Mixin(CreeperEntity.class)
 public abstract class CreeperEntityMixin {
+
+    CreeperEntity creeperEntity = ((CreeperEntity) (Object) this);
 
     /**
      * @author Luligabi
@@ -16,9 +22,15 @@ public abstract class CreeperEntityMixin {
             case "CookieCreeperEntity":
                 System.out.println("test");
                 break;
-            default: //TODO: Maintain original behavior if creeper attempting to explode isn't registered or is vanilla.
+            default:
+                Explosion.DestructionType destructionType = creeperEntity.getEntityWorld().getGameRules().getBoolean(GameRules.DO_MOB_GRIEFING) ? Explosion.DestructionType.DESTROY : Explosion.DestructionType.NONE;
+                float f = creeperEntity.shouldRenderOverlay() ? 2.0F : 1.0F;
+                creeperEntity.getEntityWorld().createExplosion(creeperEntity, creeperEntity.getX(), creeperEntity.getY(), creeperEntity.getZ(), (float) 3*f, destructionType);
                 System.out.println(this.getClass().getSimpleName());
                 break;
         }
+        ((LivingEntityAccessor) creeperEntity).setDead(true);
+        creeperEntity.remove();
+        ((CreeperEntityInvoker) creeperEntity).invokeSpawnEffectsCloud();
     }
 }
