cc5713933a0cb63c771a793156151dfc4d878744
Luligabi1 <luligabi111@gmail.com>
2021-02-23 09:39:05 -0300

    Added Cookie Creeper's finished behavior

--- src/main/java/me/luligabi/elementalcreepers/mixin/CreeperEntityMixin.java
+++ src/main/java/me/luligabi/elementalcreepers/mixin/CreeperEntityMixin.java
@@ -1,8 +1,11 @@
 package me.luligabi.elementalcreepers.mixin;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.mob.CreeperEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.world.GameRules;
 import net.minecraft.world.explosion.Explosion;
 import org.spongepowered.asm.mixin.Mixin;
@@ -18,15 +21,20 @@ public abstract class CreeperEntityMixin {
      */
     @Overwrite
     private void explode() {
+        if(creeperEntity.getEntityWorld().isClient) return;
         switch(this.getClass().getSimpleName()) {
             case "CookieCreeperEntity":
-                System.out.println("test");
+                creeperEntity.getEntityWorld().createExplosion(creeperEntity,
+                        creeperEntity.getX(), creeperEntity.getY(), creeperEntity.getZ(),
+                        0, Explosion.DestructionType.NONE);
+                creeperEntity.getEntityWorld().spawnEntity(new ItemEntity(creeperEntity.getEntityWorld(),
+                        creeperEntity.getX(), creeperEntity.getY(), creeperEntity.getZ(), new ItemStack(Items.COOKIE, 1)));
                 break;
             default:
                 Explosion.DestructionType destructionType = creeperEntity.getEntityWorld().getGameRules().getBoolean(GameRules.DO_MOB_GRIEFING) ? Explosion.DestructionType.DESTROY : Explosion.DestructionType.NONE;
-                float f = creeperEntity.shouldRenderOverlay() ? 2.0F : 1.0F;
-                creeperEntity.getEntityWorld().createExplosion(creeperEntity, creeperEntity.getX(), creeperEntity.getY(), creeperEntity.getZ(), (float) 3*f, destructionType);
-                System.out.println(this.getClass().getSimpleName());
+                creeperEntity.getEntityWorld().createExplosion(creeperEntity,
+                        creeperEntity.getX(), creeperEntity.getY(), creeperEntity.getZ(),
+                        (float) 3*(creeperEntity.shouldRenderOverlay() ? 2.0F : 1.0F), destructionType);
                 break;
         }
         ((LivingEntityAccessor) creeperEntity).setDead(true);
