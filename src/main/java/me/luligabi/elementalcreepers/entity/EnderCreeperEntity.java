5ead24f57bd21f1f827501a348ac99a2193951a1
Luligabi1 <luligabi111@gmail.com>
2021-03-03 09:20:24 -0300

    Made Ender Creepers take water damage, added translation entries.

--- src/main/java/me/luligabi/elementalcreepers/entity/EnderCreeperEntity.java
+++ src/main/java/me/luligabi/elementalcreepers/entity/EnderCreeperEntity.java
@@ -1,7 +1,14 @@
 package me.luligabi.elementalcreepers.entity;
 
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ai.goal.*;
+import net.minecraft.entity.ai.pathing.PathNodeType;
 import net.minecraft.entity.mob.CreeperEntity;
+import net.minecraft.entity.mob.EndermanEntity;
+import net.minecraft.entity.mob.EndermiteEntity;
+import net.minecraft.entity.passive.CatEntity;
+import net.minecraft.entity.passive.OcelotEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.ShulkerBulletEntity;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
@@ -11,6 +18,28 @@ public class EnderCreeperEntity extends ElementalCreeperEntity {
 
     public EnderCreeperEntity(EntityType<? extends CreeperEntity> entityType, World world) {
         super(entityType, world);
+        this.setPathfindingPenalty(PathNodeType.WATER, -1.0F);
+    }
+
+    protected void initGoals() {
+        this.goalSelector.add(0, new SwimGoal(this));
+        this.goalSelector.add(7, new WanderAroundFarGoal(this, 1.0D, 0.0F));
+        //this.goalSelector.add(1, new EndermanEntity.ChasePlayerGoal(this)); //TODO: Find way to access enderman goals.
+        //this.targetSelector.add(1, new EndermanEntity.TeleportTowardsPlayerGoal(this);
+        this.targetSelector.add(4, new UniversalAngerGoal(this, false));
+        this.goalSelector.add(2, new CreeperIgniteGoal(this));
+        this.goalSelector.add(3, new FleeEntityGoal(this, OcelotEntity.class, 6.0F, 1.0D, 1.2D));
+        this.goalSelector.add(3, new FleeEntityGoal(this, CatEntity.class, 6.0F, 1.0D, 1.2D));
+        this.goalSelector.add(4, new MeleeAttackGoal(this, 1.0D, false));
+        this.goalSelector.add(5, new WanderAroundFarGoal(this, 0.8D));
+        this.goalSelector.add(6, new LookAtEntityGoal(this, PlayerEntity.class, 8.0F));
+        this.goalSelector.add(6, new LookAroundGoal(this));
+        this.targetSelector.add(1, new FollowTargetGoal(this, PlayerEntity.class, true));
+        this.targetSelector.add(2, new RevengeGoal(this, new Class[0]));
+    }
+
+    public boolean hurtByWater() {
+        return true;
     }
 
     @Override
