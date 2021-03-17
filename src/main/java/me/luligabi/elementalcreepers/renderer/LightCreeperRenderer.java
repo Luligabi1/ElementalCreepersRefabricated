a74440410a2c607d73fb4a78f9c6a365783d6b3e
Luligabi1 <luligabi111@gmail.com>
2021-03-17 14:59:54 -0300

    Added Light Creeper.

---
+++ src/main/java/me/luligabi/elementalcreepers/renderer/LightCreeperRenderer.java
@@ -0,0 +1,18 @@
+package me.luligabi.elementalcreepers.renderer;
+
+import net.minecraft.client.render.entity.CreeperEntityRenderer;
+import net.minecraft.client.render.entity.EntityRenderDispatcher;
+import net.minecraft.entity.mob.CreeperEntity;
+import net.minecraft.util.Identifier;
+
+public class LightCreeperRenderer extends CreeperEntityRenderer {
+
+    public LightCreeperRenderer(EntityRenderDispatcher entityRenderDispatcher) {
+        super(entityRenderDispatcher);
+    }
+
+    @Override
+    public Identifier getTexture(CreeperEntity entity) {
+        return new Identifier("elementalcreepers:textures/entity/light_creeper/creeper.png");
+    }
+}
