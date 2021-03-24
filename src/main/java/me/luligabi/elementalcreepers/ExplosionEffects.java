4ed09287f6ee9c41c10a38aba4000c1bf3a35931
Luligabi1 <luligabi111@gmail.com>
2021-03-24 19:32:01 -0300

    Explosion refactor #2 - Finalized.

--- src/main/java/me/luligabi/elementalcreepers/ExplosionEffects.java
+++ src/main/java/me/luligabi/elementalcreepers/ExplosionEffects.java
@@ -1,18 +1,23 @@
 package me.luligabi.elementalcreepers;
 
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.*;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.projectile.FireworkRocketEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.nbt.StringNbtReader;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
 import net.minecraft.world.GameRules;
 import net.minecraft.world.World;
 import net.minecraft.world.explosion.Explosion;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class ExplosionEffects {
@@ -203,5 +208,106 @@ public class ExplosionEffects {
                 }
             }
         }
+    }
+    public void hydrogenExplosionEffect(Entity entity, World entityWorld, double entityX, double entityY, double entityZ) {
+        entityWorld.createExplosion(entity, entityX, entityY, entityZ,
+                (float) config.getOrDefault("hydrogenCreeperRadius", 7F), entityWorld.getGameRules().getBoolean(GameRules.DO_MOB_GRIEFING) ?
+                        Explosion.DestructionType.DESTROY : Explosion.DestructionType.NONE);
+    }
+    public void reverseExplosionEffect(Entity entity, World entityWorld, double entityX, double entityY, double entityZ) {
+        double radius = config.getOrDefault("reverseCreeperRadius", 3.75);
+
+        entityWorld.createExplosion(entity,
+                entityX, entityY, entityZ, 0, Explosion.DestructionType.NONE);
+        if(entityWorld.getGameRules().getBoolean(GameRules.DO_MOB_GRIEFING)) {
+            BlockState[][][] states = new BlockState[(int) (radius * 2 + 2)][(int) (radius * 2 + 2)][(int) (radius * 2 + 2)];
+            for (int x = (int) -radius - 1; x <= radius; x++) {
+                for (int y = (int) -radius - 1; y <= radius; y++) {
+                    for (int z = (int) -radius - 1; z <= radius; z++) {
+                        int ax = (int) (x + radius + 1);
+                        int ay = (int) (y + radius + 1);
+                        int az = (int) (z + radius + 1);
+                        double ex = entityX + x;
+                        double ey = entityY + y;
+                        double ez = entityZ + z;
+                        BlockPos blockPos = new BlockPos(ex, ey, ez);
+                        BlockState state = entityWorld.getBlockState(blockPos);
+
+                        if (state != null && state.getBlock() != null) {
+                            Block id = state.getBlock();
+
+                            if (id != Blocks.BEDROCK) {
+                                states[ax][ay][az] = null;
+
+                                if (id != null && Math.sqrt(Math.pow(x, 2.0D) + Math.pow(y, 2.0D) + Math.pow(z, 2.0D)) <= radius && ey > -1) {
+                                    states[ax][ay][az] = state;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            for (int x = (int) (-radius - 1); x <= radius; x++) {
+                for (int y = (int) (-radius - 1); y <= radius; y++) {
+                    for (int z = (int) (-radius - 1); z <= radius; z++) {
+                        BlockPos blockPos = new BlockPos(entityX + x, entityY + y, entityZ + z);
+                        BlockState state = states[(int) (x + radius + 1)][(int) (2 * radius - (y + radius))][(int) (z + radius + 1)];
+
+                        if (state != null && state.getBlock() != null && entityY + y > 5) {
+                            entityWorld.setBlockState(blockPos, state, 3);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    public void minerExplosionEffect(Entity entity, World entityWorld, double entityX, double entityY, double entityZ) {
+        entityWorld.createExplosion(entity, entityX, entityY, entityZ,
+                0, Explosion.DestructionType.NONE);
+        if(entityWorld.getGameRules().getBoolean(GameRules.DO_MOB_GRIEFING)) {
+            double radius = config.getOrDefault("minerCreeperRadius", 3.75);
+            for (int x = (int) -radius; x <= radius; x++) {
+                for (int y = (int) -radius; y <= radius; y++) {
+                    for (int z = (int) -radius; z <= radius; z++) {
+                        BlockPos blockPos = new BlockPos(entityX + x, entityY+ y, entityZ + z);
+                        BlockState state = entityWorld.getBlockState(blockPos);
+
+                        if (state != null && state.getBlock() != null) {
+                            Block stoneBlock = state.getBlock();
+                            Block[] stoneBlockList = {Blocks.STONE, Blocks.GRANITE, Blocks.DIORITE, Blocks.ANDESITE, Blocks.GRAVEL, Blocks.CLAY};
+
+                            if (stoneBlock != null && Arrays.asList(stoneBlockList).contains(stoneBlock) && Math.sqrt(Math.pow(x, 2.0D) + Math.pow(y, 2.0D) + Math.pow(z, 2.0D)) <= radius) {
+                                entityWorld.setBlockState(blockPos, Blocks.AIR.getDefaultState());
+                                if(new Random().nextBoolean()) {
+                                    entityWorld.spawnEntity(new ItemEntity(entityWorld,
+                                            entityX, entityY, entityZ, new ItemStack(stoneBlock.asItem(), 1)));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    // Missing illusion Creeper is intentional, as it's the same as vanilla creeper.
+
+    public void fireworkExplosionEffect(World entityWorld, double entityX, double entityY, double entityZ) {
+        ItemStack fireWork = new ItemStack(Items.FIREWORK_ROCKET);
+        try {
+            fireWork.putSubTag("Fireworks", StringNbtReader.parse("{Explosions:[{Type:3,Flicker:1,Colors:[I;4312372],FadeColors:[I;4312372]}],Flight:1}"));
+        } catch(CommandSyntaxException ignored) {}
+        FireworkRocketEntity fireworkEntity = new FireworkRocketEntity(entityWorld,
+                entityX, entityY, entityZ, fireWork);
+        fireworkEntity.refreshPositionAfterTeleport(entityX, entityY, entityZ);
+        entityWorld.spawnEntity(fireworkEntity);
+    }
+
+    public void cookieExplosionEffect(Entity entity, World entityWorld, double entityX, double entityY, double entityZ) {
+        entityWorld.createExplosion(entity,
+                entityX, entityY, entityZ,
+                0, Explosion.DestructionType.NONE);
+        int randomCookie = new Random().nextInt(4 - 2 + 1) + 2;
+        entityWorld.spawnEntity(new ItemEntity(entityWorld,
+                entityX, entityY, entityZ, new ItemStack(Items.COOKIE, randomCookie)));
     }
 }
--- src/main/java/me/luligabi/elementalcreepers/entity/HydrogenCreeperEntity.java
+++ src/main/java/me/luligabi/elementalcreepers/entity/HydrogenCreeperEntity.java
@@ -1,25 +1,19 @@
 package me.luligabi.elementalcreepers.entity;
 
-import me.luligabi.elementalcreepers.ElementalCreepers;
-import me.luligabi.elementalcreepers.SimpleConfig;
+import me.luligabi.elementalcreepers.ExplosionEffects;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.mob.CreeperEntity;
-import net.minecraft.world.GameRules;
 import net.minecraft.world.World;
-import net.minecraft.world.explosion.Explosion;
 
 public class HydrogenCreeperEntity extends ElementalCreeperEntity {
 
     public HydrogenCreeperEntity(EntityType<? extends CreeperEntity> entityType, World world) {
         super(entityType, world);
     }
-    SimpleConfig config = new ElementalCreepers().getConfig();
 
     @Override
     public void onExplode() {
-        this.getEntityWorld().createExplosion(this, this.getX(), this.getY(), this.getZ(),
-                (float) config.getOrDefault("hydrogenCreeperRadius", 7F), this.world.getGameRules().getBoolean(GameRules.DO_MOB_GRIEFING) ?
-                Explosion.DestructionType.DESTROY : Explosion.DestructionType.NONE);
+        new ExplosionEffects().hydrogenExplosionEffect(this, this.world, this.getX(), this.getY(), this.getZ());
         super.onExplode();
     }
 }
